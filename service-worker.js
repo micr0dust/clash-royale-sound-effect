e187dd0b4f4d0a35b2f239af24bec0c73120e611
hans chen <wuilliam104286@gmail.com>
2020-08-27 20:56:47 +0800

    cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 const filesToCache = [
     './',
-    './index.html',
     'https://code.jquery.com/jquery-3.3.1.slim.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js',
